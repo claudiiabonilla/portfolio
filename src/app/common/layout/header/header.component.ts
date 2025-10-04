b0ec001899b2156194be9ff6654ddd4f5cc8bf86
Claudia <bonillacostaclaudia@gmail.com>
2025-10-04 18:59:12 +0200

    feat(header):  diseño y funcionalidad del componente

--- src/app/common/layout/header/header.component.ts
+++ src/app/common/layout/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LanguageService } from '@app/core/services/language.service';
+import { ThemeService } from '@app/core/services/theme.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule, LogoComponent, TranslateModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  public themeService = inject(ThemeService);
+  public langService = inject(LanguageService);
+
+  toggleTheme() {
+    this.themeService.toggleDarkMode();
+  }
+
+  changeLanguage(lang: string) {
+    this.langService.setLanguage(lang);
+  }
 }
