fd6ca77a64c8259af109dead46000c93c69b0c71
Claudia <bonillacostaclaudia@gmail.com>
2025-10-06 19:53:49 +0200

    feat(about):  background del componente

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '@app/common/layout/header/logo/logo.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [LogoComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
